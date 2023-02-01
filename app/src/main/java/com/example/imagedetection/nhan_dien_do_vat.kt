1f52ad8924936b64bc53b8628e2ef9a36a4befc4
HuyPhatDS15 <huyphatle2001@gmail.com>
2023-02-01 09:51:13 +0700

    DoAnMonPTUD

--- app/src/main/java/com/example/imagedetection/nhan_dien_do_vat.kt
+++ app/src/main/java/com/example/imagedetection/nhan_dien_do_vat.kt
@@ -1,14 +1,72 @@
 package com.example.imagedetection
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.imagedetection.ml.MobilenetV110224Quant
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class nhan_dien_do_vat : AppCompatActivity() {
+
+    lateinit var bitmap: Bitmap
+    lateinit var  imgview:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nhan_dien_do_vat)
+
+        imgview = findViewById(R.id.imageView)
+        val fileName = "label.txt"
+        val inputString = application.assets.open(fileName).bufferedReader().use { it.readText() }
+        var townList = inputString.split("\n")
+
+        var tv:TextView = findViewById(R.id.textView)
+
+
+        var select: Button = findViewById(R.id.button)
+
+        select.setOnClickListener(View.OnClickListener {
+
+            var intent:Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+
+            startActivityForResult(intent, 100)
+
+        })
+
+        var predict:Button = findViewById(R.id.button2)
+        predict.setOnClickListener(View.OnClickListener {
+
+            var resized: Bitmap = Bitmap.createScaledBitmap(bitmap, 224 ,224 , true)
+            val model = MobilenetV110224Quant.newInstance(this)
+
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.UINT8)
+            var tbuffer = TensorImage.fromBitmap(resized)
+            var byteBuffer = tbuffer.buffer
+
+            inputFeature0.loadBuffer(byteBuffer)
+
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+            var max = getMax(outputFeature0.floatArray)
+
+            tv.setText(townList[max])
+
+            model.close()
+
+
+        })
 
         val home = findViewById<ImageButton>(R.id.home)
 
@@ -16,4 +74,34 @@ class nhan_dien_do_vat : AppCompatActivity() {
             startActivity(Intent(this@nhan_dien_do_vat, MainActivity::class.java))
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        imgview.setImageURI(data?.data)
+
+        var uri:Uri ?= data?.data
+
+        bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+
+    }
+
+    fun getMax(arr:FloatArray) : Int{
+
+        var ind = 0
+        var min = 0.0f
+
+        for(i in 0..1000)
+        {
+            if(arr[i]>min){
+                ind = i
+                min = arr[i]
+            }
+        }
+
+        return ind
+
+
+    }
+
 }
